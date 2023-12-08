3eb7db3ade8a4b82ea79c0ab866932cc8fcfe27f
Arely Gonzalez <arely.web@gmail.com>
2023-12-08 00:30:45 -0600

    Creando mi portafolio en react tailwild

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {HiArrowNarrowRight} from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div className=' w-full h-screen bg-gradient-to-r from-[#8B8B8B] via-white to-[#8B8B8B]'>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+
+import LOGOARR from "../assets/LOGOARR.png";
+const Navbar = () => {  
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => setShowMenu(!showMenu);
+
+    return (
+        <div className="fixed w-full h-{80px} flex justify-between items-center px-4 bg-black text-white">
+        <div>  
+            <img src={LOGOARR} alt="Logo Img"/>
+        </div>
+        {/*menu*/}
+        <ul className="hidden md:flex">
+            <li className="p-6">Inicio </li>
+            <li className="p-6">Sobre mi </li>
+            <li className="p-6">Habilidades </li>
+            <li className="p-6">Portafolio </li>
+            <li className="p-6">Contacto </li>
+        </ul>
+            {/*hamburguesa*/}
+            <div onClick={toggleMenu} className="md:hidden z-10">
+                {!showMenu ? <FaBars/> : <FaTimes/>}
+            </div>
+            {/*mobile menu*/}
+            <ul className={!showMenu ? 'hidden' : 'absolute top-10 left-0 w-full bg-black/80  flex flex-col justify-center items-center'}>
+                <li className="p-6 text-2xl">Inicio</li>
+                <li className="p-6 text-2xl">Sobre mi</li>
+                <li className="p-6 text-2xl">Habilidades</li>
+                <li className="p-6 text-2xl">Portafolio</li>
+                <li className="p-6 text-2xl">Contacto</li>
+            </ul>
+            {/*social media*/}
+            <div className="flex fixed flex-col top-[35%] left-0">
+                <ul>
+                    <li className="w-[150px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-2px] duration-300 bg-[#0077b5]">
+                        <a className="flex justify-between items-center w-full text-white " href="http://" target="_blank">
+                            Linkedin <FaLinkedin className="m-3" size={30}/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+    }
+
+export default Navbar;
